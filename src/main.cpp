e0752b6710632435a0ea0b046a61adf372c3b71b
Ellis Kesterton <31514698+eayus@users.noreply.github.com>
2025-04-28 11:15:57 +0100

    Properly cleanup window

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
-#include <iostream>
+#include <cassert>
 #include <GLFW/glfw3.h>
 
+// Configuration
+constexpr int WIDTH = 960;
+constexpr int HEIGHT = 640;
+
 int main() {
-    glfwInit();
-    GLFWwindow* window = glfwCreateWindow(1920, 1080, "OpenGL Window", nullptr, nullptr);
+    // Create Window
+    assert(glfwInit());
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Renderer", glfwGetPrimaryMonitor(), nullptr);
+    assert(window);
+
+    // Main Loop
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
+
+    // Cleanup
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
